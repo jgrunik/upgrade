746361385c8ea23fc780032e62f03fbda014e7e0
Joshua Grunik <jgrunik@gmail.com>
2023-12-18 17:50:57 +1100

    tabs for lobby scene

--- scenes/LobbyScene.tsx
+++ scenes/LobbyScene.tsx
@@ -1,5 +1,6 @@
-import { createEffect, onMount } from "solid-js";
+import { onMount } from "solid-js";
 import { useRoom } from "../contexts/room";
+import "./LobbyScene.css";
 
 export default function LobbyLayout() {
   const { room } = useRoom();
@@ -9,5 +10,18 @@ export default function LobbyLayout() {
     if (params_roomId == null) history.pushState({}, "", `?room=${room.id!}`);
   });
 
-  return <>Lobby Layout</>;
+  return (
+    <div class="row flex-spaces tabs">
+      <input id="tab1" type="radio" name="tabs" checked />
+      <label for="tab1">Lobby</label>
+      <input id="tab2" type="radio" name="tabs" />
+      <label for="tab2">Game Options</label>
+      <div class="content" id="content1">
+        lobby..
+      </div>
+      <div class="content" id="content2">
+        settings...
+      </div>
+    </div>
+  );
 }
