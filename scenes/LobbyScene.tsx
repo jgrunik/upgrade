dc50a086d40f21841cfef6532adcf95aa14ea8a1
Joshua Grunik <jgrunik@gmail.com>
2023-12-18 21:33:04 +1100

    +started lobby listing + options icon

--- scenes/LobbyScene.tsx
+++ scenes/LobbyScene.tsx
@@ -1,5 +1,6 @@
-import { onMount } from "solid-js";
+import { Index, onMount } from "solid-js";
 import { useRoom } from "../contexts/room";
+import optionsSVG from "../icons/options.svg?component-solid";
 import "./LobbyScene.css";
 
 export default function LobbyLayout() {
@@ -13,11 +14,24 @@ export default function LobbyLayout() {
   return (
     <div class="row flex-spaces tabs">
       <input id="tab1" type="radio" name="tabs" checked />
-      <label for="tab1">Lobby</label>
+      <label for="tab1">{room.players.length} : Lobby</label>
       <input id="tab2" type="radio" name="tabs" />
-      <label for="tab2">Game Options</label>
+      <label for="tab2" style={{ height: "5em" }}>
+        {
+          //@ts-expect-error
+          optionsSVG
+        }{" "}
+        Options
+      </label>
       <div class="content" id="content1">
-        lobby..
+        <h4>
+          {`${room.players.length} player${
+            room.players.length == 1 ? "" : "s"
+          } in lobby`}
+        </h4>
+        <Index each={room.players}>
+          {(player, i) => <li>{player().id}</li>}
+        </Index>
       </div>
       <div class="content" id="content2">
         settings...
